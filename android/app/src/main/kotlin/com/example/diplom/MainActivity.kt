e76aa3a3bbeffd543e97d9c613e66bc26ca500f3
chan0nel <shvakova2004@gmail.com>
2022-12-20 13:16:35 +0300

    main: remove waste devices, add some depencities

--- android/app/src/main/kotlin/com/example/diplom/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/diplom/MainActivity.kt
@@ -1,6 +1,14 @@
 package com.example.diplom
 
-import io.flutter.embedding.android.FlutterActivity
+import com.yandex.mapkit.MapKitFactory;
+import io.flutter.embedding.engine.FlutterEngine;
+import io.flutter.embedding.android.FlutterActivity;
+import androidx.annotation.NonNull;
 
-class MainActivity: FlutterActivity() {
+
+class MainActivity : FlutterActivity() {
+  override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+    MapKitFactory.setApiKey("f9146513-4739-4a0a-9546-52727633b82e"); // Your generated API key
+    super.configureFlutterEngine(flutterEngine);
+  }
 }
