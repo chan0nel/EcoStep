2a31ca132e682b45986b92743ffb493470cd3f00
chan0nel <shvakova2004@gmail.com>
2023-04-10 17:39:17 +0300

    add flutter map

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_map/flutter_map.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -10,6 +11,16 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: FlutterMap(
+        options: MapOptions(),
+        children: [
+          TileLayer(
+            urlTemplate:
+                'https://tile.jawg.io/jawg-sunny/{z}/{x}/{y}.png?access-token=43LAhxnCITdWbjRocDbg5csEq5LaIYqxcn1TLZX2mSI0ngLlFmDmfR4Tq9UNTRaM',
+          ),
+        ],
+      ),
+    );
   }
 }
